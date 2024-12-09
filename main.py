d285f323cf26e57f74909c40e10e1ea2af67ee91
L. Graff <lgraff@annavanrijn.nl>
2024-12-09 19:18:56 +0100

    bug fix

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 print("Ik zal de stelling van pythagoras voor je gebruiken om de afstand tussen twee punten te berekenen.")
 #Gegevens vragen.
-x1 = input("Wat is de x-coördinaat van het eerste punt?")
-y1 = input("Wat is de y-coördinaat van het eerste punt?")
-x2 = input("Wat is de x-coördinaat van het tweede punt?")
-y2 = input("Wat is de y-coördinaat van het tweede punt?")
+x1 = float(input("Wat is de x-coördinaat van het eerste punt?"))
+y1 = float(input("Wat is de y-coördinaat van het eerste punt?"))
+x2 = float(input("Wat is de x-coördinaat van het tweede punt?"))
+y2 = float(input("Wat is de y-coördinaat van het tweede punt?"))
 
 #Afstand berekenen.
 import wiskunde
