0ed57712135e92c6b5d3cf8552f89343f46d3d7f
L. Graff <lgraff@annavanrijn.nl>
2024-12-09 17:44:19 +0100

    Ik heb een functie om het favoriete vak te checken toegevoegd aan main.py

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 naam = input("Wat is je naam?")
 print(f"Hallo {naam}")
+# Hier wordt gekeken of de gebruiker het juiste favoriete schoolvak heeft.
+fav_vak = input("Wat is je favoriete schoolvak?")
+if fav_vak.lower() == "informatica":
+    print("Goed zo!")
