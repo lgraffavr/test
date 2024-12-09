ce887627391f88055b7fcc67d05681499c733c19
lgraffavr <lgraff@annavanrijn.nl>
2024-12-09 18:50:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ print(f"Hallo {naam}")
 # Hier wordt gekeken of de gebruiker het juiste favoriete schoolvak heeft.
 fav_vak = input("Wat is je favoriete schoolvak?")
 if fav_vak.lower() == "informatica":
-    print("Goed zo!")
+    print("Goed zo bro!")
